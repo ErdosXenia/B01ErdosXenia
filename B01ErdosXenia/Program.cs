ff9d2f9844f7880f0e2a163a7db21782f8834961
ErdosXenia <erdos.xenia@gmail.com>
2020-09-14 11:04:44 +0200

    Beolvasas

--- B01ErdosXenia/Program.cs
+++ B01ErdosXenia/Program.cs
@@ -3,13 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace B01ErdosXenia
 {
     class Program
     {
+        static void Beolvasas()
+        {
+            StreamReader sr = new StreamReader("adatok.dat");
+            while (!sr.EndOfStream)
+            {
+                string[] adat = sr.ReadLine().Split(' ');
+                int[] a = new int[1000];
+
+                for (int i = 0; i < a.Length; i++)
+                {
+                    a[i] = int.Parse(adat[i]);
+                }
+            }
+            sr.Close();
+        }
+
+        static void Minimumertek()
+        {
+            
+        }
+
         static void Main(string[] args)
         {
+            Beolvasas();
+            Minimumertek();
+
+            Console.ReadKey();
         }
+
+
     }
 }
